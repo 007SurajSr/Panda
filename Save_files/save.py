9292bc010f9a60e8edadd405c6cc480eb1b507b5
Suraj Gupta <8700suraj@gmail.com>
2026-08-13 12:59:02 +0530

    pip

--- Save_files/save.py
+++ Save_files/save.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
 data = {
-    "Name": ['Ram','Shyam','Ghanshyam'],
-    "Age" : [10,20,30],
+    "Name": ['Ram', 'Shyam', 'Ghanshyam'],
+    "Age": [10, 20, 30],
     "City": ['Nagpur', 'Mumbai', 'Delhi']
 }
 
 df = pd.DataFrame(data)
 print(df)
 
+# Save to CSV
 df.to_csv("Output.csv", index=False)
+
+# Save to Excel (requires 'openpyxl') #pip install openpyxl
 df.to_excel("Output.xlsx", index=False)
-df.to_json("Output.json", index=False)
+
+# Save to JSON (use orient='records' for a clean list of objects)
+df.to_json("Output.json", orient="records", indent=4)
